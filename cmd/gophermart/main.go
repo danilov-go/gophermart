06e09a6bc91d88c0ec598018593a0ee0468003c3
sergeydanilove93 <serdge88@gmail.com>
2026-06-24 06:18:49 +0300

    Init server, config and zap logger

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"github.com/danilov-go/gophermart/internal/config"
+	"github.com/danilov-go/gophermart/internal/logger"
+	"github.com/danilov-go/gophermart/internal/server"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	configs := config.ConfigServer{
+		Net: config.NetAddress{
+			Host: "localhost",
+			Port: 8080,
+		},
+		DatabaseUri:   "host=localhost user=gophermart password=123 dbname=gophermart sslmode=disable",
+		AccrualAddres: "",
+	}
+	if err := logger.Initialize("info"); err != nil {
+		panic(err)
+	}
+	configs.Get()
+	r := chi.NewRouter()
+	serv := server.New(configs.Net.String(), logger.Log.Sugar(), r)
+	if err := serv.Run(); err != nil {
+		panic(err)
+	}
+}
---
+++ internal/server/server.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"net/http"
+)
+
+type log interface {
+	Infow(msg string, keysAndValues ...any)
+}
+
+type Server struct {
+	Server *http.Server
+	Logger log
+}
+
+func New(port string, l log, r http.Handler) *Server {
+	server := &http.Server{
+		Addr:    port,
+		Handler: r,
+	}
+	return &Server{
+		Server: server,
+		Logger: l,
+	}
+}
+
+func (serv *Server) Run() error {
+	serv.Logger.Infow("Running server", "address", serv.Server.Addr)
+	return serv.Server.ListenAndServe()
+}
