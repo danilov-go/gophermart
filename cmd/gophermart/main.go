5e9be2740d9b3b4c56cc243d1782a4a8074c3162
sergeydanilove93 <serdge88@gmail.com>
2026-07-08 06:49:24 +0300

    Add gzip middleware

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -44,6 +44,7 @@ func main() {
 	h := handler.NewHandlers(storage, logger.Log.Sugar())
 	r := chi.NewRouter()
 	r.Use(middleware.StripSlashes)
+	r.Use(handler.GzipMiddleware)
 	r.Route("/api/user", func(r chi.Router) {
 		r.Post("/register", h.RegisterUser(configs.Key))
 		r.Post("/login", h.LoginUser(configs.Key))
